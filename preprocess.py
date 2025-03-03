2c122a21a271cccebefca309b601a5fb45365889
misilelaboratory <misileminecord@gmail.com>
2025-03-04 03:19:44 +0900

    Revert "refactor: autotokenizer -> electratokenizer and trying to add cuda"
    This reverts commit 36506f6e095f9e9b27f3ab95e509692c4be31cc4.

--- preprocess.py
+++ preprocess.py
@@ -6,16 +6,14 @@ from libraries.scrape import read, Data
 from libraries.data import clean
 
 from torch import Tensor
-from torch.cuda import is_available
-from transformers import ElectraTokenizer # pyright: ignore[reportMissingTypeStubs]
+from transformers import AutoTokenizer # pyright: ignore[reportMissingTypeStubs]
 from tqdm import tqdm
 
-tokenizer: ElectraTokenizer = ElectraTokenizer.from_pretrained('beomi/kcELECTRA-base') # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
+tokenizer = AutoTokenizer.from_pretrained('beomi/kcELECTRA-base') # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
 data = read("data.avro")
 datas: tuple[list[dict[str, list[Tensor]]], list[int]] = ([], [])
 max_len = 128
 amount = 3
-device = "cuda" if is_available() else "cpu"
 
 # just for data collection
 real_len: list[int] = []
@@ -31,18 +29,18 @@ for _i in t:
     data = clean(j)
     if data.replace('\n', '').strip() == '':
       continue
-    encoded: dict[str, Tensor] = tokenizer( # pyright: ignore[reportArgumentType, reportAssignmentType, reportUnknownMemberType]
+    encoded: dict[str, Tensor] = tokenizer( # pyright: ignore[reportUnknownVariableType]
       data, max_length=max_len, padding='max_length', truncation=True, return_tensors='pt'
-    ).to(device)
-    input_ids: Tensor = encoded['input_ids'].squeeze()
-    attention_mask: Tensor = encoded['attention_mask'].squeeze()
-    t_data['input_ids'].append(input_ids)
-    t_data['attention_mask'].append(attention_mask)
+    )
+    input_ids: Tensor = encoded['input_ids'].squeeze() # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
+    attention_mask: Tensor = encoded['attention_mask'].squeeze() # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
+    t_data['input_ids'].append(input_ids) # pyright: ignore[reportUnknownArgumentType]
+    t_data['attention_mask'].append(attention_mask) # pyright: ignore[reportUnknownArgumentType]
     count += 1
-    if max(len(input_ids[input_ids != blank_tokenizer]), len(input_ids[input_ids != blank_tokenizer])) == max_len:
+    if max(len(input_ids[input_ids != blank_tokenizer]), len(input_ids[input_ids != blank_tokenizer])) == max_len: # pyright: ignore[reportUnknownArgumentType]
       skipped += 1
       continue
-    avg_len = (len(input_ids[input_ids != blank_tokenizer]) + len(input_ids[input_ids != blank_tokenizer])) // 2
+    avg_len = (len(input_ids[input_ids != blank_tokenizer]) + len(input_ids[input_ids != blank_tokenizer])) // 2 # pyright: ignore[reportUnknownArgumentType]
     real_len.append(avg_len)
     t.set_description_str(f"{sum(real_len) / len(real_len):.3f}, {real_len.count(max_len)}, {skipped}")
   if count < amount:
