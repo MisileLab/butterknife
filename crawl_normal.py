6692729a649c9af65b8c313aa5c7f8f25fcf0929
misilelab <misileminecord@gmail.com>
2025-03-08 18:45:54 +0900

    fix: crawl normal load error
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- crawl_normal.py
+++ crawl_normal.py
@@ -108,7 +108,7 @@ async def subroutine(i: dict[str, object], df: DataFrame, retry: int = 0) -> Dat
 
 async def main():
   df = read("user.avro")
-  for i in df.select(col('suicidal')).to_dicts():
+  for i in df.select(col('user_type') == 1).to_dicts():
     df = await subroutine(i, df)
   df.write_avro("user.avro")
 
