92ee4d8ff3f7c9d3a64f0cebfdaa90933a33fb01
misilelab <misileminecord@gmail.com>
2025-03-11 01:44:16 +0900

    feat: add proper instagram login
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- scrapers/instagram/lib.py
+++ scrapers/instagram/lib.py
@@ -1,5 +1,4 @@
-from time import sleep
-
+from pathlib import Path
 from instagrapi import Client # pyright: ignore[reportMissingTypeStubs]
 from instagrapi.exceptions import ( # pyright: ignore[reportMissingTypeStubs]
   ClientConnectionError,
@@ -27,10 +26,24 @@ silent_error: list[type[Exception]] = [
 ]
 
 client = Client(proxy=get_proxy()) # pyright: ignore[reportArgumentType]
+accounts: list[tuple[str, str]] = []
+len_accounts = len(accounts)
+account_index = 0
+for i in Path("accounts.txt").read_text().split("\n"):
+  data = i.split(":")
+  accounts.append((i[0], i[1]))
 
 def silent_errors(error: Exception):
   if type(error) in silent_error:
     logger.error(f"Silent error: {error}")
-    sleep(60 * 6)
+    login_resp = False
+    while not login_resp:
+      global account_index
+      account_index += 1
+      if account_index == len_accounts:
+        account_index = 0
+      login_resp = client.login(accounts[account_index][0], accounts[account_index][1])
+    logger.info(f"login to {accounts[account_index][0]}")
+    return
   raise error
 
