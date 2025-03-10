6fcb1947560c80c86f79fbb9c9d8de9fd215573a
misilelab <misileminecord@gmail.com>
2025-03-11 01:53:10 +0900

    fix: SyntaxError
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- scrapers/instagram/lib.py
+++ scrapers/instagram/lib.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 from pathlib import Path
+
 from instagrapi import Client # pyright: ignore[reportMissingTypeStubs]
 from instagrapi.exceptions import ( # pyright: ignore[reportMissingTypeStubs]
   ClientConnectionError,
@@ -25,10 +27,14 @@ silent_error: list[type[Exception]] = [
   SentryBlock
 ]
 
+@dataclass
+class Initalizer:
+  account_index = 0
+
 client = Client(proxy=get_proxy()) # pyright: ignore[reportArgumentType]
 accounts: list[tuple[str, str]] = []
 len_accounts = len(accounts)
-account_index = 0
+initalizer = Initalizer()
 for i in Path("accounts.txt").read_text().split("\n"):
   if i.strip() == "":
     continue
@@ -39,15 +45,17 @@ def silent_errors(error: Exception):
   if type(error) in silent_error:
     logger.error(f"Silent error: {error}")
     login_resp = False
-    org_accounts_index = account_index
-    while not login_resp and org_accounts_index != account_index:
-      global account_index
-      account_index += 1
-      if account_index == len_accounts:
-        account_index = 0
-      login_resp = client.login(accounts[account_index][0], accounts[account_index][1])
+    org_accounts_index = initalizer.account_index
+    while not login_resp and org_accounts_index != initalizer.account_index:
+      initalizer.account_index += 1
+      if initalizer.account_index == len_accounts:
+        initalizer.account_index = 0
+      login_resp = client.login(
+        accounts[initalizer.account_index][0],
+        accounts[initalizer.account_index][1]
+      )
     if login_resp:
-      logger.info(f"login to {accounts[account_index][0]}")
+      logger.info(f"login to {accounts[initalizer.account_index][0]}")
     else:
       logger.error("all accounts are blocked")
       raise error
