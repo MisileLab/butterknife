1eefdff004d48923c5c5bc93dd1aaa2d6b842fac
misilelab <misileminecord@gmail.com>
2025-03-13 03:13:33 +0900

    Revert "feat: add instagram scraper, powered by hikerapi"
    This reverts commit 874d7d562168f89dd2e2dcdd315fe0a89bf835b2.

--- scrapers/instagram/crawl_suicidal.py
+++ scrapers/instagram/crawl_suicidal.py
@@ -1,10 +1,9 @@
+from loguru import logger
+
 from time import sleep
 from secrets import SystemRandom
 
-from loguru import logger
-from pydantic import BaseModel
-
-from .lib import client
+from .lib import client, silent_errors
 from libraries.scrape import Provider, UserType, is_unique_user, read, append, User
 
 suicidals = [
@@ -13,26 +12,27 @@ suicidals = [
   "죽고싶다", "죽고싶어"
 ]
 
-class InternalUser(BaseModel):
-  pk: str | int
-  id: str | int | None
-  username: str | None
 
 async def main():
   df = read("user.avro")
 
   for suicidal_tag in suicidals:
     logger.info(suicidal_tag)
-    data = client.hashtag_medias_top(suicidal_tag, count=30) # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
-    data.extend(client.hashtag_medias_recent(suicidal_tag, count=30)) # pyright: ignore[reportUnknownMemberType]
-    for post in data: # pyright: ignore[reportUnknownVariableType]
-      user = InternalUser.model_validate(post["user"])
+    try:
+      data = client.hashtag_medias_top(suicidal_tag, 30)
+      data.extend(client.hashtag_medias_recent(suicidal_tag, 30))
+    except Exception as e:
+      silent_errors(e)
+      data = client.hashtag_medias_top(suicidal_tag, 30)
+      data.extend(client.hashtag_medias_recent(suicidal_tag, 30))
+    for post in data:
+      user = post.user
       logger.info(user.username)
-      userid = str(user.pk)
+      userid = user.pk
       if is_unique_user(df, userid, Provider.instagram):
         df = append(df, User(
           uid=userid,
-          name="" if user.username is None else user.username,
+          name=user.pk,
           user_type=UserType.suicidal,
           provider=Provider.instagram
         ))
